740566ff3cec46d26192be6eec65e68fc5f4f037
Dalen <dalen.chan@hotmail.com>
2021-10-06 11:09:04 +0100

    Node mod removed

--- session-8/npm-intro/index.js
+++ session-8/npm-intro/index.js
@@ -1,1 +1,9 @@
+// Use this syntax when using lodash library
+import _ from 'lodash';
+
 console.log("Inside index.js S8");
+
+// Lodash forEach
+_.each([1, 2, 3, 4], function(num) {
+    console.log(num);
+});
