4e224cfeee7488e57773fe29622d40c04d120559
Dalen <dalen.chan@hotmail.com>
2021-10-11 16:31:05 +0100

    String calculator

---
+++ session-11/challenges/stringCalc.js
@@ -0,0 +1,13 @@
+/* String calculator (TDD)
+Casio would like to introduce a calculator prorgram that takes a string and can perform 
+the add operation on all numbers provided: 
+
+1. An empty string should return 0
+2. A single number returns the value 
+3. Two numbers comma delimited returns the sum
+4. Support multiple delimiters to separate the numbers betwen the number. Again the sum 
+should still be return. ~1 or ,
+5. Handle more than 2 numbers 
+6. Passing negative numbers will be handled by producing an error. 
+7. Numbers bigger than 1000 should be ignored */
+
