eb263f52b174997a11423bf6e2d668459ceb2d51
Dalen <dalen.chan@hotmail.com>
2021-10-12 16:12:03 +0100

    Connect travel blogs to firebase DB

--- session-12/react-app/travel-blogger/src/pages/AllTravelBlogs.js
+++ session-12/react-app/travel-blogger/src/pages/AllTravelBlogs.js
@@ -1,31 +1,68 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 import TravelBlogList from '../components/blogs/TravelBlogList';
 
-// Mock data 
-const BLOG_DATA = [
-    { 
-        id: 1,
-        title: "This is my first trip to Darwen",
-        image: 'https://www.lancashiretelegraph.co.uk/resources/images/2814496?type=responsive-gallery-fullscreen',
-        address: 'Darwen Town Centre',
-        city: 'Also known as "Darren"',
-        description: 'Number 2 town in the UK.',
-    },
-    {
-        id: 2,
-        title: "This is my first trip to Bolton",
-        image: 'https://www.theboltonnews.co.uk/resources/images/2700631/',
-        address: 'Bolton Town Centre',
-        city: 'Bolton',
-        description: 'Number 1 town in the UK.',
-    },
-];
+// // Mock data 
+// const BLOG_DATA = [
+//     { 
+//         id: 1,
+//         title: "This is my first trip to Darwen",
+//         image: 'https://www.lancashiretelegraph.co.uk/resources/images/2814496?type=responsive-gallery-fullscreen',
+//         address: 'Darwen Town Centre',
+//         city: 'Also known as "Darren"',
+//         description: 'Number 2 town in the UK.',
+//     },
+//     {
+//         id: 2,
+//         title: "This is my first trip to Bolton",
+//         image: 'https://www.theboltonnews.co.uk/resources/images/2700631/',
+//         address: 'Bolton Town Centre',
+//         city: 'Bolton',
+//         description: 'Number 1 town in the UK.',
+//     },
+// ];
 
 const AllTravelBlogs = () => {
+
+
+    const [ isLoading, setIsloading ] = useState(true);
+    const [ travelBlogs, setTravelblogs ] = useState([]);
+
+    useEffect(() => {
+        const apiURL = process.env.REACT_APP_TRAVEL_API_URL;
+
+        fetch(apiURL)
+        .then(res => {
+            return res.json();
+        })
+
+        .then((data) => {
+            const travelBlogs = [];
+
+            for (const key in data) {
+                const travelBlog = {
+                    id: key,
+                    ...data[key],
+                };
+
+                travelBlogs.push(travelBlog);
+            }
+
+            setIsloading(false);
+            setTravelblogs(travelBlogs);
+
+        });
+    }, []);
+
+    if (isLoading) {
+        return (<section>
+            <p>Loading .... </p>
+        </section>)
+    }
+
     return (
         <section>
             <h1>All Travel Blogs</h1>
-            <TravelBlogList blogs={BLOG_DATA}/>
+            <TravelBlogList blogs={travelBlogs} />
 
         </section>
     );
