69ff2a8892363f25d0f3e1d5feceb5683baab622
Dalen <dalen.chan@hotmail.com>
2021-10-12 11:08:25 +0100

    Setup app.js

--- session-12/react-app/travel-blogger/src/App.js
+++ session-12/react-app/travel-blogger/src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import { Route } from 'react-router-dom';
+// Import pages 
+import AllTravelBlogs from './pages/AllTravelBlogs';
+import BucketList from './pages/BucketList';
+import NewTravelBlog from './pages/NewTravelBlog';
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <Route>
+        <AllTravelBlogs />
+      </Route>
+
+      <Route>
+        <NewTravelBlog />
+      </Route>
+
+      <Route>
+        <BucketList />
+      </Route>
+    </div>
   );
 }
 
