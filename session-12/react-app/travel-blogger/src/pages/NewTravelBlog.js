2206ac6ec6d0e1b22af6f9249cb9abeac50be773
Dalen <dalen.chan@hotmail.com>
2021-10-12 15:42:21 +0100

    Added .env variable

--- session-12/react-app/travel-blogger/src/pages/NewTravelBlog.js
+++ session-12/react-app/travel-blogger/src/pages/NewTravelBlog.js
@@ -4,13 +4,21 @@ import NewTravelBlogForm from '../components/blogs/NewTravelBlogForm';
 const NewTravelBlog = () => {
 
     const addNewTravelBlogHandler = (newBlogData) => {
-        fetch('https://react-travel-blogger-default-rtdb.europe-west1.firebasedatabase.app/travelblogs.json', {
+        const apiURL = process.env.REACT_APP_TRAVEL_API_URL;
+
+        console.log(apiURL);
+        fetch(apiURL, {
+
             method: 'POST',
+            
             body: JSON.stringify(newBlogData),
+
             // Headers: telling server metadata about the data it is receiving
             headers: {
                 'Content-Type': 'application/json'
             },
+
+
         });
     };
 
